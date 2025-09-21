5a8870d05383bd961e1e6545130ea40cc64d8781
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-09-21 21:33:38 +0100

    Add new favicon images in various formats (16x16, 32x32, and ICO)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import "./globals.css"
+import Head from "next/head"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+      </Head>
       <body className={`${poppins.variable} ${poppins.className} font-sans antialiased`}>
         <Suspense fallback={<div>Loading...</div>}>
           {children}
